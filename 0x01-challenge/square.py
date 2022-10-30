897f8489138cc64c115749951bee2ebba8238f01
MarufAbrar <maracode1@gmail.com>
2022-10-30 11:37:15 +0300

    alx is fun

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 class square():
+    
     width = 0
     height = 0
 
+
     def __init__(self, *args, **kwargs):
+
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -16,12 +19,12 @@ class square():
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        print("height: ", self.height)
         return "{}/{}".format(self.width, self.height)
 
-
 if __name__ == "__main__":
-
-    s = square(width=12, height=9)
+    square.height = 2
+    s = square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
